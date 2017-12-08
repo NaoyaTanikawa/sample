59aa9f81cee63fb05de43d1ae4fa7cd98975590b
_your_name <you@example.com>
2017-12-08 17:57:49 +0900

    need files commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const iconv = new Iconv('SHIFT_JIS','UTF-8');
 
 var str = 'お試しcode\n';
 var http = require("http");
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
     response.end(str);
