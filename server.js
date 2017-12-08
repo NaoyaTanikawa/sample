8cc6a44069724dbc577e2c1ae83d2051aa528334
_your_name <you@example.com>
2017-12-08 17:46:36 +0900

    sample create

---
+++ server.js
@@ -0,0 +1,14 @@
+const Iconv = require('iconv').Iconv;
+const iconv = new Iconv('SHIFT_JIS','UTF-8');
+
+var str = 'お試しcode\n';
+var http = require("http");
+const PORT = 3001;
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+    response.end(str);
+}).listen(PORT);
+
+console.log('Server is runnning at http://localhost:' + PORT + '/');
+
+//process.exit();
